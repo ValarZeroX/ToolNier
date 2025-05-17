8cda54f98972a289a94a71e9bd9307677386c86b
Valar <azgrhl@gmail.com>
2025-05-17 10:50:34 +0800

    add footer

--- components/Footer/FooterLinks.tsx
+++ components/Footer/FooterLinks.tsx
@@ -1,95 +1,103 @@
 import { FC } from 'react';
-import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from '@tabler/icons-react';
-import { ActionIcon, Container, Group, Text } from '@mantine/core';
+import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube, IconTools } from '@tabler/icons-react';
+import { ActionIcon, Container, Group, Text, rem } from '@mantine/core';
 // import { MantineLogo } from '@mantinex/mantine-logo';
 import classes from './FooterLinks.module.css';
 
 const data = [
-  {
-    title: 'About',
-    links: [
-      { label: 'Features', link: '#' },
-      { label: 'Pricing', link: '#' },
-      { label: 'Support', link: '#' },
-      { label: 'Forums', link: '#' },
-    ],
-  },
-  {
-    title: 'Project',
-    links: [
-      { label: 'Contribute', link: '#' },
-      { label: 'Media assets', link: '#' },
-      { label: 'Changelog', link: '#' },
-      { label: 'Releases', link: '#' },
-    ],
-  },
-  {
-    title: 'Community',
-    links: [
-      { label: 'Join Discord', link: '#' },
-      { label: 'Follow on Twitter', link: '#' },
-      { label: 'Email newsletter', link: '#' },
-      { label: 'GitHub discussions', link: '#' },
-    ],
-  },
+    {
+        title: 'About',
+        links: [
+            { label: 'Features', link: '#' },
+            { label: 'Pricing', link: '#' },
+            { label: 'Support', link: '#' },
+            { label: 'Forums', link: '#' },
+        ],
+    },
+    {
+        title: 'Project',
+        links: [
+            { label: 'Contribute', link: '#' },
+            { label: 'Media assets', link: '#' },
+            { label: 'Changelog', link: '#' },
+            { label: 'Releases', link: '#' },
+        ],
+    },
+    {
+        title: 'Community',
+        links: [
+            { label: 'Join Discord', link: '#' },
+            { label: 'Follow on Twitter', link: '#' },
+            { label: 'Email newsletter', link: '#' },
+            { label: 'GitHub discussions', link: '#' },
+        ],
+    },
 ];
 
 interface FooterLinksProps {
-  lng: string;
+    lng: string;
 }
 
-const FooterLinks: FC<FooterLinksProps> = ({lng }) => {
-  const groups = data.map((group) => {
-    const links = group.links.map((link, index) => (
-      <Text<'a'>
-        key={index}
-        className={classes.link}
-        component="a"
-        href={link.link}
-        onClick={(event) => event.preventDefault()}
-      >
-        {link.label}
-      </Text>
-    ));
+const FooterLinks: FC<FooterLinksProps> = ({ lng }) => {
+    const groups = data.map((group) => {
+        const links = group.links.map((link, index) => (
+            <Text<'a'>
+                key={index}
+                className={classes.link}
+                component="a"
+                href={link.link}
+                onClick={(event) => event.preventDefault()}
+            >
+                {link.label}
+            </Text>
+        ));
+
+        return (
+            <div className={classes.wrapper} key={group.title}>
+                <Text className={classes.title}>{group.title}</Text>
+                {links}
+            </div>
+        );
+    });
 
     return (
-      <div className={classes.wrapper} key={group.title}>
-        <Text className={classes.title}>{group.title}</Text>
-        {links}
-      </div>
+        <footer className={classes.footer}>
+            <Container className={classes.inner}>
+
+                <div className={classes.logo}>
+                    <Group>
+                        <IconTools
+                            style={{ width: rem(36), height: rem(36) }}
+                            stroke={1.5}
+                            color="var(--mantine-color-blue-filled)"
+                        />
+                        <div>ToolNier</div>
+                    </Group>
+                    <Text size="xs" c="dimmed" className={classes.description}>
+                        Build fully functional accessible web applications faster than ever
+                    </Text>
+                </div>
+                <div className={classes.groups}>{groups}</div>
+            </Container>
+            <Container className={classes.afterFooter}>
+                <Text c="dimmed" size="sm">
+                    © 2020 toolnier.com. All rights reserved.
+                </Text>
+
+                <Group gap={0} className={classes.social} justify="flex-end" wrap="nowrap">
+                    <ActionIcon size="lg" color="gray" variant="subtle">
+                        <IconBrandTwitter size={18} stroke={1.5} />
+                    </ActionIcon>
+                    <ActionIcon size="lg" color="gray" variant="subtle">
+                        <IconBrandYoutube size={18} stroke={1.5} />
+                    </ActionIcon>
+                    <ActionIcon size="lg" color="gray" variant="subtle">
+                        <IconBrandInstagram size={18} stroke={1.5} />
+                    </ActionIcon>
+                </Group>
+            </Container>
+        </footer>
     );
-  });
-
-  return (
-    <footer className={classes.footer}>
-      <Container className={classes.inner}>
-        <div className={classes.logo}>
-          {/* <MantineLogo size={30} /> */}
-          <Text size="xs" c="dimmed" className={classes.description}>
-            Build fully functional accessible web applications faster than ever
-          </Text>
-        </div>
-        <div className={classes.groups}>{groups}</div>
-      </Container>
-      <Container className={classes.afterFooter}>
-        <Text c="dimmed" size="sm">
-          © 2020 mantine.dev. All rights reserved.
-        </Text>
-
-        <Group gap={0} className={classes.social} justify="flex-end" wrap="nowrap">
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandTwitter size={18} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandYoutube size={18} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandInstagram size={18} stroke={1.5} />
-          </ActionIcon>
-        </Group>
-      </Container>
-    </footer>
-  );
 }
 
 export default FooterLinks;
